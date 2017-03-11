168428984f6abe11cce23fdaacbedb3047440c0a
lanwenhong <hexiexuanlv@126.com>
2017-03-11 16:54:00 +0800

    fixbug

--- bin/urls.py
+++ bin/urls.py
@@ -1,6 +1,13 @@
 #coding: utf-8
 
+from handler import store
+
 urls = (
-
-
+    ('^/store/v1/page/index$', store.StoreH5Main),
+    ('^/store/v1/page/eye_sight$', store.StoreH5EyeSight),
+    ('^/store/v1/page/train$', store.StoreH5Train),
+    ('^/store/v1/page/dev$', store.StoreH5Dev),
+    ('^/store/v1/page/bill$', store.StoreH5Bill),
+    ('^/store/v1/page/dis_record$', store.StoreH5DisRecord),
+    ('^/store/v1/page/dis_comsumer_info$', store.StoreH5DisComsumerInfo),
 )
