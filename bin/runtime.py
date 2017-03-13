e6863b03d757b30dad85d750122c52dab04b71b1
lanwenhong <hexiexuanlv@126.com>
2017-03-13 19:43:31 +0800

    fixbug

---
+++ bin/runtime.py
@@ -0,0 +1,16 @@
+#coding: utf-8
+
+import os, sys
+HOME = os.path.dirname(os.path.abspath(__file__))
+from zbase.base import loader
+
+loader.loadconf_argv(HOME)
+import config
+import redis
+
+class Runtime:
+    def __init__(self):
+        self.redis_pool = redis.ConnectionPool.from_url(config.redis_url)
+
+global g_rt
+g_rt = Runtime()
