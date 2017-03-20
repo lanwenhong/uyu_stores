e6816f65ed7bd3767e6dbb1fdbf877a625563d62
zhougang <zhougang@qfpay.com>
2017-03-20 17:19:48 +0800

    修改忽略文件的路径

---
+++ bin/static/js/uyusys/bill.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mac on 17/3/15.
+ */
+require(['../require-config'], function() {
+    require(["zepto", "yangzheng", "ajax_rule","vue"],function($, yangzheng, ajax_rule, vue){
+        $(document).ready(function() {
+        });
+    });
+});
