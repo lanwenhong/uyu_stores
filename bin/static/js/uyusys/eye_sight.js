bad60b4419da4c96887d71664fca7f613d2384ac
zhougang <zhougang@qfpay.com>
2017-03-23 15:56:26 +0800

    修改字段

--- bin/static/js/uyusys/eye_sight.js
+++ bin/static/js/uyusys/eye_sight.js
@@ -2,7 +2,7 @@
  * Created by mac on 17/3/15.
  */
 require(['../require-config'], function() {
-    require(["zepto", "ajax_rule", "jquerySwipe"],function($, ajax_rule, jquerySwipe){
+    require(["zepto", "ajax_rule", "vue"],function($, ajax_rule, vue){
         $(document).ready(function() {
 
             $(".eye_sight").swipe({
@@ -13,18 +13,30 @@ require(['../require-config'], function() {
 
             var page = 1;
 
-            $('.eye_sight_delete').on('click', function () {
-                alert("dddddvvvv");
-                var listReq = {
-                    page: ''+page,
-                    maxnum: '10',
-                    se_userid:localStorage.getItem("userid")
-                };
-                ajax_rule.ajax_rule('/store/v1/api/eyesight_list', 'GET', 'json', listReq, '.zheceng', function (respData) {
-                    page = page + 1;
-                    alert(JSON.stringify(respData));
+            function createVueWithUserId(store_user_id) {
+                var vukk = new vue({
+                    el: '#wap',
+                    data: {eye_sights:[],
+                    },
+                    created: function () {
+                        this.fetch_store_info();//获取店铺数据
+                    },
+                    methods: {
+                        fetch_store_info: function () {
+                            var getInfoData = {
+                                se_userid: store_user_id,
+                                userid: store_user_id
+                            };
+                            ajax_rule.ajax_rule('/store/v1/api/store_info', 'GET', 'json', getInfoData, '.zheceng', function (respData) {
+                                var eyesightArr = respData['info'];
+                                for (var i = 0; i < eyesightArr.length; i++){
+                                    this.eye_sights.push(eyesightArr[i]);
+                                }
+                            });
+                        },
+                    }
                 });
-            })
+            }
 
 
         });
