65178c84860fac15f14802049be491a9513c2965
zhougang <zhougang@qfpay.com>
2017-03-23 18:49:59 +0800

    修改列表

--- bin/static/js/uyusys/dev.js
+++ bin/static/js/uyusys/dev.js
@@ -2,8 +2,38 @@
  * Created by mac on 17/3/15.
  */
 require(['../require-config'], function() {
-    require(["zepto", "yangzheng", "ajax_rule","vue"],function($, yangzheng, ajax_rule, vue){
+    require(["zepto", "ajax_rule","vue"],function($, ajax_rule, vue){
         $(document).ready(function() {
+
+            function createVueWithUserId(store_user_id) {
+                var vukk = new vue({
+                    el: '#wap',
+                    data: {
+                        devices: new Array(),
+                    },
+                    created: function () {
+                        this.fetch_store_info();//获取店铺数据
+                    },
+                    methods: {
+                        fetch_store_info: function () {
+                            var _this = this;
+                            var listReq = {
+                                se_userid: store_user_id,
+                                maxnum: "10",
+                                page:''+page
+                            };
+                            ajax_rule.ajax_rule('/store/v1/api/device_list', 'GET', 'json', listReq, '.zheceng', function (respData) {
+                                var devArr = respData['info'];
+                                for (var i = 0; i < devArr.length; i++){
+                                    _this.devices.push(devArr[i]);
+                                }
+                            });
+                        },
+                    }
+                });
+            }
+            createVueWithUserId(localStorage.getItem("userid"));
+
         });
     });
 });
--- bin/static/js/uyusys/eye_sight.js
+++ bin/static/js/uyusys/eye_sight.js
@@ -4,25 +4,20 @@
 require(['../require-config'], function() {
     require(["zepto", "ajax_rule", "vue"],function($, ajax_rule, vue){
         $(document).ready(function() {
-
-            $(".eye_sight").swipe({
-                swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-                    alert(direction);
-                }
-            });
-
             var page = 1;
 
             function createVueWithUserId(store_user_id) {
                 var vukk = new vue({
                     el: '#wap',
-                    data: {eye_sights:[],
+                    data: {
+                        eye_sights: new Array(),
                     },
                     created: function () {
                         this.fetch_store_info();//获取店铺数据
                     },
                     methods: {
                         fetch_store_info: function () {
+                            var _this = this;
                             var listReq = {
                                 se_userid: store_user_id,
                                 maxnum: "10",
@@ -31,8 +26,7 @@ require(['../require-config'], function() {
                             ajax_rule.ajax_rule('/store/v1/api/eyesight_list', 'GET', 'json', listReq, '.zheceng', function (respData) {
                                 var eyesightArr = respData['info'];
                                 for (var i = 0; i < eyesightArr.length; i++){
-                                    alert("kkk");
-                                    this.eye_sights.push(eyesightArr[i]);
+                                    _this.eye_sights.push(eyesightArr[i]);
                                 }
                             });
                         },
