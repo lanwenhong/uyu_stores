03f201ee1306912850d88383ec0e074a2b7efe5c
zhougang <zhougang@qfpay.com>
2017-03-23 21:36:31 +0800

    nothing

--- bin/static/js/uyusys/eye_sight.js
+++ bin/static/js/uyusys/eye_sight.js
@@ -62,6 +62,8 @@ require(['../require-config'], function() {
                     registeUpdateView();
                 });
             }
+            registeUpdateView();
+
 
         });
     });
