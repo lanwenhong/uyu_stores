23626a8ca7a8d6bcebda8835b71df23cc9c9f59d
zhougang <zhougang@qfpay.com>
2017-03-24 16:16:42 +0800

    删除验证

--- bin/static/js/uyusys/train.js
+++ bin/static/js/uyusys/train.js
@@ -2,7 +2,7 @@
  * Created by mac on 17/3/15.
  */
 require(['../require-config'], function() {
-    require(["zepto", "yangzheng", "ajax_rule","vue"],function($, yangzheng, ajax_rule, vue){
+    require(["zepto", "ajax_rule","vue"],function($, ajax_rule, vue){
         $(document).ready(function() {
         });
     });
