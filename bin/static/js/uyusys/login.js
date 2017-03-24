c2faf8a59ad786b40c2bd55ab56d8425517702fa
zhougang <zhougang@qfpay.com>
2017-03-24 11:29:00 +0800

    nothing

--- bin/static/js/uyusys/login.js
+++ bin/static/js/uyusys/login.js
@@ -9,7 +9,6 @@ require(['../require-config'], function() {
                     mobile: '17889807865',
                     password: '807865'
                 };
-
                 ajax_rule.ajax_rule('/store/v1/api/login', 'POST', 'json', loginReq, '.zheceng', function (respData) {
                     alert("登录成功");
                     localStorage.setItem("userid", respData['userid']);
