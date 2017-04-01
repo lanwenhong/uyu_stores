c06e9419715cd5e37625ae7bab5cfa17ae2ff30a
zhougang <zhougang@qfpay.com>
2017-04-01 15:15:42 +0800

    去试光师页面

--- bin/static/js/uyusys/eye_sight.js
+++ bin/static/js/uyusys/eye_sight.js
@@ -5,21 +5,6 @@ require(['../require-config'], function() {
     require(["zepto", "ajax_rule", "vue", "native"],function($, ajax_rule, vue, native){
         $(document).ready(function() {
 
-            native.regNativeCallJS("addEyeSight", function (cb) {
-                alert("添加石光师");
-            });
-            var rightConfig = {
-                type:"jsfunc",
-                title:"添加",
-                funcName:"addEyeSight"
-            };
-            native.addRightBtn(rightConfig, function (cb) {
-                console.log(cb.ret);
-            });
-
-
-
-
             var page = 1;
             var vukk = new vue({
                 el: '#wap',
@@ -71,6 +56,47 @@ require(['../require-config'], function() {
                     }
                 }
             });
+
+            native.regNativeCallJS("addEyeSight", function (cb) {
+                //去添加试光师的页面
+                native.regNativeCallJS("notiUpdateEyesightView", function () {
+                    page = 1;
+                    var len = vukk.eye_sights.length;
+                    vukk.eye_sights.splice(0, len);
+                    vukk.next_list_page();
+                });
+
+                var regNoti = {
+                    notiName:"updateEyesightWebView",
+                    notiRespFuncName:"notiUpdateEyesightView"
+                };
+                native.regNotifiaction(regNoti, function () {
+                    var openUrl = location.protocol + '//' + location.host + '/store/v1/page/eye_sight_info.html';
+
+                    if (window.WebViewJavascriptBridge !== "undefined") {
+                        var openUrlData ={
+                            url:openUrl,
+                            pullDown:"0",
+                            pullUp:"0"
+                        };
+                        native.openUrl(openUrlData, function (cb) {
+                            console.log("call back");
+                        });
+                    } else {
+                        alert("location.href");
+                        location.href = openUrl;
+                    }
+                });
+            });
+            var rightConfig = {
+                type:"jsfunc",
+                title:"添加",
+                funcName:"addEyeSight"
+            };
+            native.addRightBtn(rightConfig, function (cb) {
+                console.log(cb.ret);
+            });
+
             //注册下拉刷新方法
             native.updateCurrentView(function () {
                 page = 1;
