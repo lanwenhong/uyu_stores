a80067187fda8ac7d46b5516d8646c1bf9634ad0
richard-deng <richard.deng@live.com>
2017-04-14 10:42:58 +0800

    添加测试

--- bin/test/test.py
+++ bin/test/test.py
@@ -26,12 +26,12 @@ class TestUyuStores(unittest.TestCase):
     def test_login(self):
         self.url = '/store/v1/api/login'
         self.send = {
-            "mobile": "13475481254",
-            "new_password": hashlib.md5('123456').hexdigest(),
+            # "mobile": "13475481254",
+            "mobile": "13802438733",
+            "new_password": hashlib.md5('438733').hexdigest(),
             "old_password": '123456'
         }
         ret = self.client.post(self.url, self.send)
-        # log.info(ret)
         print '--headers--'
         print self.client.client.headers
         respcd = json.loads(ret).get('respcd')
@@ -44,7 +44,6 @@ class TestUyuStores(unittest.TestCase):
         self.send = {"se_userid": 51561, "userid": 51561}
         headers = {'cookie': 'sessionid=7dcf5197-5413-4690-87f5-dd26315110a7'}
         ret = self.client.get(self.url, self.send, headers=headers)
-        # log.info(ret)
         respcd = json.loads(ret).get('respcd')
         self.assertEqual(respcd, '0000')
 
@@ -85,7 +84,7 @@ class TestUyuStores(unittest.TestCase):
         self.assertEqual(respcd, '0000')
 
 
-    #@unittest.skip("skipping")
+    @unittest.skip("skipping")
     def test_pass_change(self):
         self.url = '/store/v1/api/passwd_change'
         self.send = {
@@ -97,6 +96,71 @@ class TestUyuStores(unittest.TestCase):
         log.info(ret)
         respcd = json.loads(ret).get('respcd')
         self.assertEqual(respcd, '0000')
+
+
+    @unittest.skip("skipping")
+    def test_eyesight_list(self):
+        self.url = '/store/v1/api/eyesight_list'
+        self.send = {'page': 1, 'maxnum': 10, 'se_userid': 51561}
+        headers = {'cookie': 'sessionid=38126e96-5c05-4c4e-96e8-bd846b455daf'}
+        ret = self.client.get(self.url, self.send, headers=headers)
+        log.info(ret)
+        respcd = json.loads(ret).get('respcd')
+        self.assertEqual(respcd, '0000')
+
+
+    @unittest.skip("skipping")
+    def test_device_list(self):
+        self.url = '/store/v1/api/device_list'
+        self.send = {'page': 1, 'maxnum': 10, 'se_userid': 51561}
+        headers = {'cookie': 'sessionid=38126e96-5c05-4c4e-96e8-bd846b455daf'}
+        ret = self.client.get(self.url, self.send, headers=headers)
+        log.info(ret)
+        respcd = json.loads(ret).get('respcd')
+        self.assertEqual(respcd, '0000')
+
+
+    @unittest.skip("skipping")
+    def test_store_allocate_list(self):
+        self.url = '/store/v1/api/store_allocate_list'
+        self.send = {'page': 1, 'maxnum': 10, 'se_userid': 51561}
+        headers = {'cookie': 'sessionid=38126e96-5c05-4c4e-96e8-bd846b455daf'}
+        ret = self.client.get(self.url, self.send, headers=headers)
+        log.info(ret)
+        respcd = json.loads(ret).get('respcd')
+        self.assertEqual(respcd, '0000')
+
+
+    @unittest.skip("skipping")
+    def test_store_settle_list(self):
+        self.url = '/store/v1/api/settle_list'
+        self.send = {'page': 1, 'maxnum': 10, 'se_userid': 51561}
+        headers = {'cookie': 'sessionid=38126e96-5c05-4c4e-96e8-bd846b455daf'}
+        ret = self.client.get(self.url, self.send, headers=headers)
+        log.info(ret)
+        respcd = json.loads(ret).get('respcd')
+        self.assertEqual(respcd, '0000')
+
+
+    @unittest.skip("skipping")
+    def test_eyesight_info(self):
+        self.url = '/store/v1/api/eyesight'
+        self.send = {'phone_num': '13475481254', 'se_userid': 51561}
+        headers = {'cookie': 'sessionid=38126e96-5c05-4c4e-96e8-bd846b455daf'}
+        ret = self.client.get(self.url, self.send, headers=headers)
+        log.info(ret)
+        respcd = json.loads(ret).get('respcd')
+        self.assertEqual(respcd, '0000')
+
+
+    #@unittest.skip("skipping")
+    def test_eyesight_bind(self):
+        self.url = '/store/v1/api/eyesight'
+        self.send = {'userid': '1217', 'se_userid': 51561}
+        headers = {'cookie': 'sessionid=38126e96-5c05-4c4e-96e8-bd846b455daf'}
+        ret = self.client.post(self.url, self.send, headers=headers)
+        log.info(ret)
+        respcd = json.loads(ret).get('respcd')
 
 
 
